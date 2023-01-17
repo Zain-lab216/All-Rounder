751c96d7bed78761fede5d7bf6ff7d9490c56ba4
Zain <zain4u2000@gmail.com>
2023-01-17 16:54:06 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let hamberger = document.querySelector('.hamberger')
+let times = document.querySelector('.times')
+let mobileNav = document.querySelector('.mobile-nav')
+
+
+
+
+function showNav(){
+   mobileNav.classList.add('show')
+}
+function hideNav(){
+   mobileNav.classList.remove('show')
+}
